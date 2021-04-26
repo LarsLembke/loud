69461479eb88d4ed08cf51f752a8a22db88a34fa
LarsLembke <lars46i6@stud.kea.dk>
2021-04-26 12:02:14 +0200

    cfghbjnk

--- page-om-loud.php
+++ page-om-loud.php
@@ -15,29 +15,41 @@ get_header();
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
 
-            <div id="splash_omos">
-                <div id="beskrivelse_boks_omos">
-                    <div id="omos_beskrivelse"></div>
+
+            <section id="firstsection">
+                <div id="beskrivelse_boks_front">
+                    <h1>
+						Dine <b class="red">favorit</b> podcasts
+					</h1>
+                    <h3 class="haandskrevet">
+						...lige når du har lyst!
+					</h3>
+                    <div class="front-icons">
+
+                        <a href="#"><img src="http://lembkesites.dk/kea/09_CMS/loud/wp-content/uploads/2021/04/appstore_8e70a99f.svg"></a>
+                        <a href="#"><img src="http://lembkesites.dk/kea/09_CMS/loud/wp-content/uploads/2021/04/google-play-2_a914298c.png"></a>
+                    </div>
+                    <div id="front_beskrivelse"></div>
                 </div>
-                <div id="img_container_omos">
-                    <img src="" alt="" id="splash_image_omos">
+                <div id="img_container_front">
+                    <div id="splash_image_front"></div>
                 </div>
-            </div>
-            <section id="first_section_omos">
-                <article id="txt_omos_beskrivelse"></article>
-                <div id="img_omos_firstcontainer"></div>
             </section>
             <section id="second_section_omos">
+                <article id="txt_omos_beskrivelse"></article>
+                <div id="img_omos_secondcontainer"></div>
+            </section>
+            <section id="third_section_omos">
                 <div id="omos_gridcontainer">
                     <img src="" alt="" class="img_omos_grid">
                     <img src="" alt="" class="img_omos_grid">
                     <img src="" alt="" class="img_omos_grid">
                     <img src="" alt="" class="img_omos_grid">
                 </div>
-                <div id="txt_omos_secondcontainer"></div>
-            </section>
-            <section id="third_section_omos">
                 <div id="txt_omos_thirdcontainer"></div>
+            </section>
+            <section id="fourth_section_omos">
+                <div id="txt_omos_fourthcontainer"></div>
                 <div id="link_gridcontainer">
                     <a href="" class="link_omos_grid"></a>
                     <a href="" class="link_omos_grid"></a>
--- page.php
+++ page.php
@@ -12,15 +12,46 @@
 get_header();
 ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<section id="firstsection">
+                <div id="beskrivelse_boks_front">
+					<h1>
+						Dine <b class="red">favorit</b> podcasts
+					</h1>
+					<h3 class="haandskrevet">
+						...lige når du har lyst!
+					</h3>
+					<div class="front-icons">
 
+					<a href="#"><img src="http://lembkesites.dk/kea/09_CMS/loud/wp-content/uploads/2021/04/appstore_8e70a99f.svg"></a>
+						<a href="#"><img src="http://lembkesites.dk/kea/09_CMS/loud/wp-content/uploads/2021/04/google-play-2_a914298c.png"></a>
+						</div>
+                    <div id="front_beskrivelse"></div>
+                </div>
+                <div id="img_container_front">
+                    <div id="splash_image_front"></div>
+                </div>
+            </section>
 
+			<?php
 
-        </main>
-        <!-- #main -->
-    </div>
-    <!-- #primary -->
+			// Start the Loop.
+			while ( have_posts() ) :
+				the_post();
 
-    <?php
+				get_template_part( 'template-parts/content/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
 get_footer();
